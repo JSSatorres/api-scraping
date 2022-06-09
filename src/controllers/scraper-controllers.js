e2121b2d84b99f5429830eba27723f833aae164a
JSSatorres <juansataz@gmail.com>
2022-06-10 01:35:25 +0200

    function scraped ok whitout comments

---
+++ src/controllers/scraper-controllers.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+
+async function scrap(req, res, next) {
+  // const { bookId } = req.params;
+
+  try {
+    (async () => {
+
+      let dataTitleUrl = [];
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.goto("https://news.ycombinator.com/");
+
+      const listTitleUrl = await page.$$(".athing");
+
+      for (const item of listTitleUrl) {
+        const title = await item.$(".titlelink");
+        const url = await item.$(".sitestr");
+        const getTitle = await page.evaluate(title=> title?.innerText,title);
+        const getUrl = await page.evaluate((url) => url?.innerText, url);
+        dataTitleUrl.push({ title: getTitle, url: getUrl ,num:"1"});
+      }
+
+      const dataAuthorComents =[]
+      const listAuthorComents = await page.$$(".subtext");
+
+      for (const item of listAuthorComents) {
+        const author = await item.$(".hnuser");
+        // const urls = await item.$eval(" span",el=>el.textContent);
+  
+        const getAuthor = await page.evaluate(author=> author?.innerText,author);
+        // const getUrl = await page.evaluate(comments => comments?.innerText, comments);
+        dataAuthorComents.push({ author: getAuthor,comments: "null" , control:"1" });
+      }
+
+      const info = dataTitleUrl.map((titleComent, index) => {
+        return {title: titleComent?.title,url: titleComent?.url, author: dataAuthorComents[index]?.author, comments: dataAuthorComents[index]?.comments};
+      });
+
+      await browser.close();
+
+      res.status(200).send({
+        data: info,
+      });
+    })();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { scrap};
