7ca1c78b21aa5319a4ed311673280b425648ece1
JSSatorres <juansataz@gmail.com>
2022-06-12 10:43:00 +0200

    saving and extracting info in the cache

--- src/controllers/scraper-controllers.js
+++ src/controllers/scraper-controllers.js
@@ -1,10 +1,20 @@
 const puppeteer = require("puppeteer");
 const redis = require("redis");
-const util = require("util");
+// const util = require("util");
 
 async function scrapOnePage(req, res, next) {
 
   try {
+    const client = redis.createClient("redis://127.0.0.1:6379");
+
+    // client.set = util.promisify(client.set);
+    await client.connect()
+    const cachedPost1 = await client.get( `info1`);
+
+    if (cachedPost1 ) {
+      return res.send({ data: JSON.parse(cachedPost1)});
+    }
+
     (async () => {
       const browser = await puppeteer.launch({ headless: false });
       const page = await browser.newPage();
@@ -39,16 +49,19 @@ async function scrapOnePage(req, res, next) {
         });
       }
 
+      await browser.close();
+
       const info = dataTitleUrl.map((titleComent, index) => {
         return {
           title: titleComent?.title,
           url: titleComent?.url,
           author: dataAuthorComents[index]?.author,
           comments: dataAuthorComents[index]?.comments,
+          control:index,
         };
       });
 
-      await browser.close();
+      client.set(`info1`, JSON.stringify(info));
       res.status(200).send({
         data: info,
       });
@@ -62,28 +75,44 @@ async function scrapOnePage(req, res, next) {
 async function scrapXPages(req, res, next) {
   const { pages } = req.params;
   const client = redis.createClient("redis://127.0.0.1:6379");
+  let pagesNoScrap = 0
 
-  client.set = util.promisify(client.set);
+  // const SET_ASYNC = util.promisify(client.set).bind(client);
+  // const GET_ASYNC = util.promisify(client.get).bind(client);
+  // const HGETALL_ASYNC = util.promisify(client.hGetAll).bind(client);
+
   await client.connect()
-  const cachedPost = await client.get( `info${pages}`);
-  
 
-  if (cachedPost ) {
-    return res.send({ data: JSON.parse(cachedPost)});
+  const cachePage = await client.get(`info${pages}`);
+  if (cachePage ) {
+    return res.send({ data: JSON.parse(cachePage)});
+  }
+
+  for (let index =pages-1; 1 <index; index--) {
+
+    let checkPages = "info"+index
+    console.log("check pages ",checkPages);
+    const cachePage = await client.get(checkPages);
+    if (cachePage) {
+      pagesNoScrap = index
+      break;
+    }
   }
 
   let loop;
   if (pages === undefined) {
     loop = 0;
+
   } else {
-    loop = parseInt(pages);
+    loop = parseInt(pages)-pagesNoScrap;
+    console.log("esto ea un loop", loop)
   }
 
   try {
     (async () => {
       const browser = await puppeteer.launch({ headless: false });
       const page = await browser.newPage();
-      await page.goto("https://news.ycombinator.com/");
+      await page.goto(`https://news.ycombinator.com/news?p=${pagesNoScrap}`);
       let totalDataTitleUrl = [];
       let totalDataAuthorComents = [];
 
@@ -135,9 +164,22 @@ async function scrapXPages(req, res, next) {
           control:index,
         };
       });
-      client.set(`info${pages}`, JSON.stringify(info));
+
+
+      const previousCachePage = await client.get(`info${pagesNoScrap}`);
+      const format = await JSON.parse(previousCachePage)
+
+      let totalInfo =[]
+      console.log("pagesNoScrap",pagesNoScrap);
+      if (pagesNoScrap !== 0 ) {
+        totalInfo= await format.concat(info)
+      }else{
+        totalInfo= info
+      }
+      client.set(`info${pages}`, JSON.stringify(totalInfo));
+      console.log("info pages",`info${pages}`);
       res.status(200).send({
-        data: info,
+        data: totalInfo,
       });
     })();
 
